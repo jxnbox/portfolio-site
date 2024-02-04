6dfba22e76cf448cfc5cc08f2626dea091f5d1de
jxnbox <janr3p@gmail.com>
2024-02-04 18:51:46 +0000

    adding navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,36 @@
+import Toolbar from '@mui/material/Toolbar';
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+const Nav = () => {
+    const pages = ["About", "Projects", "Contact"]
+
+    return (
+        <AppBar position="static">
+            <Container>
+                <Toolbar disableGutters>
+                    {pages.map(page => {
+                        return <Typography
+                        variant='h6'
+                        noWrap
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                        >
+                        {page}
+                        </Typography>
+                    })}
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+
+export default Nav;
